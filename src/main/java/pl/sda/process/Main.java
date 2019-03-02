b125a59568d9128804e7c0a8a793c41317e11716
motylla <magda.waliszewska@gmail.com>
2019-03-02 12:46:47 +0100

    opening rtf files in Java

--- src/main/java/pl/sda/process/Main.java
+++ src/main/java/pl/sda/process/Main.java
@@ -1,8 +1,42 @@
 package pl.sda.process;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Process process;
+        String homeDir = System.getProperty("user.home");
+        System.out.println(homeDir);
+
+
+        try {
+            process = Runtime.getRuntime().exec("ls -ltra " + homeDir);
+
+            System.out.println(process.getOutputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            System.out.println(reader);
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            process = Runtime.getRuntime().exec("open " + homeDir + "/plik.rtf");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
